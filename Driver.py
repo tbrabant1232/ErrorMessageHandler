5f76e63d4ae1fc06a5418a86c0773d0c19cc8921
tbrabant1232 <60270599+tbrabant1232@users.noreply.github.com>
2020-01-31 15:53:15 -0800

    Add files from local workstation

---
+++ Driver.py
@@ -0,0 +1,86 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import json as json
+import csv
+import urllib.request
+import sys
+from datetime import date
+
+today = date.today()
+dateStr = today.strftime("%d-%b-%Y")
+csvfile = (dateStr + "raw.csv")
+
+def get_responses(paths: list, fetch: str) -> list:
+    output_list = []
+    if fetch == 'file':
+        for path in paths:
+            handle = open(path.strip('\n'), 'r')
+            output_list.append(handle)
+    elif fetch == 'url':
+        for path in paths:
+            handle = get_url_response(path)
+    else:
+        raise ValueError("Unsupported Fetch type")
+    return output_list
+
+
+def get_url_response(url: str):
+    return url
+
+
+def get_error_from_json(handle):
+    json_object = json.load(handle)
+    errors = list(find_json("error_message", json_object))
+    if errors:
+        return errors
+
+
+def get_json_paths(path: str):
+    handle = open(path, 'r')
+    lines = handle.readlines()
+    return lines
+
+
+def find_json(key, collection):
+    bad_chars = [',', "\"", "\n"]
+    if isinstance(collection, dict):
+        for k in collection.items():
+            if k[0] == key:
+                yield k[1]
+            elif isinstance(k[1], dict):
+                for result in find_json(key, k[1]):
+                    yield result
+            elif isinstance(k[1], list):
+                for d in k[1]:
+                    for result in find_json(key, d):
+                        yield result
+    elif isinstance(collection, list):
+        for value in collection:
+            for r in find_json(key, value):
+                for i in bad_chars :
+                    r = r.replace(i, '')
+                yield r
+
+
+def main():
+    paths = get_json_paths("C:\\Users\\chq-teresab\\Desktop\\links.txt")
+    responses = get_responses(paths, 'file')
+    errors = []
+    for response in responses:
+        errors.append(get_error_from_json(response))
+    print(errors)
+
+    res = []
+    for val in errors:
+        if val != None :
+            res.append(val)
+
+    with open(csvfile, 'w') as file:
+     writer = csv.writer(file, escapechar='\\', delimiter="\n", quoting=csv.QUOTE_NONE,)
+     writer.writerows(res)
+    print("Created csv file", csvfile)
+
+
+if __name__ == "__main__":
+    main()
---
+++ app.py
@@ -0,0 +1,39 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+from datetime import date
+
+today = date.today()
+dateStr = today.strftime("%d-%b-%Y")
+csvFile = (dateStr + ".csv")
+inputFile = "C:\\Users\\chq-teresab\\Desktop\\31-Jan-2020.csv"
+
+df = pd.read_csv(inputFile)
+
+
+def generate_table(dataframe, max_rows=40):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+
+
+    )
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H4('Master Consol RELEASE Build Failures '+ dateStr),
+    generate_table(df)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
